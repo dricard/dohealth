3432442173d3477e33f50c86f03928b8f0107ced
Denis Ricard <d.ricard@me.com>
2018-05-27 17:29:24 -0400

    refactor to extract tags processing into functions

--- DoHealthLogger.swift
+++ DoHealthLogger.swift
@@ -22,6 +22,58 @@ let dayOneJournal = "santé"
 
 let defaultTags = ["dohealth", "santé" ]
 
+/* ********************************* */
+
+
+// requires Swift 4.0
+
+
+//-- get parameter input
+// `argument` holds the text entered in Alfred by the user
+// I initialize it with an example of something the user could enter
+// for testing. Use the various examples to test different cases.
+// In real use the argument will be replaced with what the user typed of course
+
+// var argument = "-l cetone @0.1 mmol/l"
+// var argument = "-m matin @vitamines B complex, B2, D, C, Omega-3"
+// var argument = "-s soir @ballonements"
+// var argument = "-w pm @700 ml d'eau"
+// var argument = "-t fête @gâteau au chocolat SG"
+ var argument = "-t 2 @saumon avec salade de kale et avocats"
+// var argument = "-t 1 @œuf, avocats et café"
+// var argument = "-t 3 @bœuf avec asperges et champignons"
+// var argument = "-t 0 @noisettes et amandes"
+
+// these test arguments are missing the '@' character
+// var argument = "-l cetone 0.1 mmol/l"
+// var argument = "-m matin vitamines B complex, B2, D, C, Omega-3"
+// var argument = "-s soir ballonements"
+// var argument = "-w pm 700 ml d'eau"
+// var argument = "-t fête gâteau au chocolat SG"
+// var argument = "yogourt 0% avec amandes"
+
+
+#if swift(>=4.0)
+	if CommandLine.arguments.count > 1 {
+		argument = CommandLine.arguments[1]
+	}
+#elseif swift(>=1.0)
+	print("Unsupported version of Swift (<= 4.0) please update to Swift 4.0")
+	break
+#endif
+
+// MARK: - Properties
+
+// variable 'entryText' will hold the food passed in
+
+var entryText  = ""
+
+// `outputString` is the result of the script that will be passed to the CLI, 
+// we initialize it with the Day One CLI command, setting the default journal
+// and the default tags.
+
+var outputString: String = "dayone2 --journal "
+
 enum EntryType: String {
 	case food = "mangé:"
 	case symptom = "symptôme:"
@@ -82,53 +134,42 @@ func entryType(for argument: String) -> (EntryType, Bool) {
 	}
 }
 
-/* ********************************* */
+// MARK: - Utilities
 
+func replaceSpaces(in tag: String) -> String {
+	return tag.replacingOccurrences(of: "_", with: "\\ ")
+}
 
-// requires Swift 4.0
+func removeSpecialCharacters(in tag: String) -> String {
+	// escape special characters
+	// ! ? $ % # & * ( ) blank tab | ' ; " < > \ ~ ` [ ] { }
+	var returnedTag = tag
+	returnedTag = returnedTag.replacingOccurrences(of: "!", with: "\\!")
+	returnedTag = returnedTag.replacingOccurrences(of: "?", with: "\\?")
+	returnedTag = returnedTag.replacingOccurrences(of: "$", with: "\\$")
+	returnedTag = returnedTag.replacingOccurrences(of: "%", with: "\\%")
+	returnedTag = returnedTag.replacingOccurrences(of: "#", with: "\\#")
+	returnedTag = returnedTag.replacingOccurrences(of: "&", with: "\\&")
+	returnedTag = returnedTag.replacingOccurrences(of: "*", with: "\\*")
+	returnedTag = returnedTag.replacingOccurrences(of: "(", with: "\\(")
+	returnedTag = returnedTag.replacingOccurrences(of: ")", with: "\\)")
+	returnedTag = returnedTag.replacingOccurrences(of: "|", with: "\\|")
+	returnedTag = returnedTag.replacingOccurrences(of: "'", with: "\\'")
+	returnedTag = returnedTag.replacingOccurrences(of: ";", with: "\\;")
+	returnedTag = returnedTag.replacingOccurrences(of: "<", with: "\\<")
+	returnedTag = returnedTag.replacingOccurrences(of: ">", with: "\\>")
+	returnedTag = returnedTag.replacingOccurrences(of: "\\", with: "\\\\")
+	returnedTag = returnedTag.replacingOccurrences(of: "~", with: "\\~")
+	returnedTag = returnedTag.replacingOccurrences(of: "`", with: "\\`")
+	returnedTag = returnedTag.replacingOccurrences(of: "[", with: "\\[")
+	returnedTag = returnedTag.replacingOccurrences(of: "]", with: "\\]")
+	returnedTag = returnedTag.replacingOccurrences(of: "{", with: "\\{")
+	returnedTag = returnedTag.replacingOccurrences(of: "}", with: "\\}")
 
+	return returnedTag
+}
 
-//-- get parameter input
-// `argument` holds the text entered in Alfred by the user
-// I initialize it with an example of something the user could enter
-// for testing. Use the various examples to test different cases.
-// In real use the argument will be replaced with what the user typed of course
-
-// var argument = "-l cetone @0.1 mmol/l"
-// var argument = "-m matin @vitamines B complex, B2, D, C, Omega-3"
-// var argument = "-s soir @ballonements"
-// var argument = "-w pm @700 ml d'eau"
-// var argument = "-t fête @gâteau au chocolat SG"
-
-// these test arguments are missing the '@' character
-// var argument = "-l cetone 0.1 mmol/l"
-// var argument = "-m matin vitamines B complex, B2, D, C, Omega-3"
-// var argument = "-s soir ballonements"
-// var argument = "-w pm 700 ml d'eau"
-// var argument = "-t fête gâteau au chocolat SG"
- var argument = "yogourt 0% avec amandes"
-
-
-#if swift(>=4.0)
-	if CommandLine.arguments.count > 1 {
-		argument = CommandLine.arguments[1]
-	}
-#elseif swift(>=1.0)
-	print("Unsupported version of Swift (<= 4.0) please update to Swift 4.0")
-	break
-#endif
-
-// MARK: - Properties
-
-// variable 'entryText' will hold the food passed in
-
-var entryText  = ""
-
-// `outputString` is the result of the script that will be passed to the CLI, 
-// we initialize it with the Day One CLI command, setting the default journal
-// and the default tags.
-
-var outputString: String = "dayone2 --journal "
+// MARK: - Process
 
 // add journal name and default tags
 
@@ -138,8 +179,6 @@ for defaulTag in defaultTags {
 	let tag = defaulTag.replacingOccurrences(of: " ", with: "\\ ")
 	outputString += tag + " "
 }
-
-// MARK: - Process input
 
 // check type of entry and if we need to process tags
 
@@ -161,7 +200,7 @@ if hasTags {
 		// omitted
 		
 		let tags = String(argument.prefix(upTo: endOfTags)).split(separator: " ").map{ String($0) }
-		
+
 		// Now process the entryText part to remove the end of tags marker
 		
 		// get the entryText part of the input
@@ -184,53 +223,28 @@ if hasTags {
 		// Add the tags to the output string separated by spaces
 		// skipping the first one which is the `-t` marker
 			
-		for i in 1..<tags.count {
+		for tag in tags.dropFirst() {
 			
 			// first we process underscores (_) in tags to replace them with escaped spaces so they're
 			// treated as a single tag
-			
-			var tag = tags[i].replacingOccurrences(of: "_", with: "\\ ")
-			
-			// escape special characters
-			// ! ? $ % # & * ( ) blank tab | ' ; " < > \ ~ ` [ ] { }
-				
-			tag = tags[i].replacingOccurrences(of: "!", with: "\\!")
-			tag = tags[i].replacingOccurrences(of: "?", with: "\\?")
-			tag = tags[i].replacingOccurrences(of: "$", with: "\\$")
-			tag = tags[i].replacingOccurrences(of: "%", with: "\\%")
-			tag = tags[i].replacingOccurrences(of: "#", with: "\\#")
-			tag = tags[i].replacingOccurrences(of: "&", with: "\\&")
-			tag = tags[i].replacingOccurrences(of: "*", with: "\\*")
-			tag = tags[i].replacingOccurrences(of: "(", with: "\\(")
-			tag = tags[i].replacingOccurrences(of: ")", with: "\\)")
-			tag = tags[i].replacingOccurrences(of: "|", with: "\\|")
-			tag = tags[i].replacingOccurrences(of: "'", with: "\\'")
-			tag = tags[i].replacingOccurrences(of: ";", with: "\\;")
-			tag = tags[i].replacingOccurrences(of: "<", with: "\\<")
-			tag = tags[i].replacingOccurrences(of: ">", with: "\\>")
-			tag = tags[i].replacingOccurrences(of: "\\", with: "\\\\")
-			tag = tags[i].replacingOccurrences(of: "~", with: "\\~")
-			tag = tags[i].replacingOccurrences(of: "`", with: "\\`")
-			tag = tags[i].replacingOccurrences(of: "[", with: "\\[")
-			tag = tags[i].replacingOccurrences(of: "]", with: "\\]")
-			tag = tags[i].replacingOccurrences(of: "{", with: "\\{")
-			tag = tags[i].replacingOccurrences(of: "}", with: "\\}")
+			var processedTag = replaceSpaces(in: tag)
+			processedTag = removeSpecialCharacters(in: processedTag)
 
 			// here we replace the shorthands for breakfast, lunch, etc.
 			
-			if tags[i] == "1" {
-				tag = "déjeuner"
-			} else if tags[i] == "2" {
-				tag = "dîner"
-			} else if tags[i] == "3" {
-				tag = "souper"
-			} else if tags[i] == "0" {
-				tag = "collation"
+			if tag == "1" {
+				processedTag = "déjeuner"
+			} else if tag == "2" {
+				processedTag = "dîner"
+			} else if tag == "3" {
+				processedTag = "souper"
+			} else if tag == "0" {
+				processedTag = "collation"
 			}
 			
 			// add this processed tag to the output string
 			
-			outputString += tag + " "
+			outputString += processedTag + " "
 		}
 	}
 			
